8a9a06f038bc31b6eabbf83dfcdf4066ff3ac8b7
祁威德 <dq042000@gmail.com>
2023-09-20 15:07:59 +0800

    Add new album.

--- web/api/module/Album/src/Model/Album.php
+++ web/api/module/Album/src/Model/Album.php
@@ -2,16 +2,88 @@
 
 namespace Album\Model;
 
+use DomainException;
+use Laminas\InputFilter\InputFilter;
+use Laminas\Validator\StringLength;
+
 class Album
 {
     public $id;
     public $artist;
     public $title;
 
+    private $inputFilter;
+
     public function exchangeArray(array $data)
     {
         $this->id     = !empty($data['id']) ? $data['id'] : null;
         $this->artist = !empty($data['artist']) ? $data['artist'] : null;
         $this->title  = !empty($data['title']) ? $data['title'] : null;
+    }
+
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+        throw new DomainException(sprintf(
+            '%s does not allow injection of an alternate input filter',
+            __CLASS__
+        ));
+    }
+
+    public function getInputFilter()
+    {
+        if ($this->inputFilter) {
+            return $this->inputFilter;
+        }
+
+        $inputFilter = new InputFilter();
+
+        $inputFilter->add([
+            'name' => 'id',
+            'required' => true,
+            'filters' => [
+                ['name' => 'int'],
+            ],
+        ]);
+        
+        $inputFilter->add([
+            'name' => 'artist',
+            'required' => true,
+            'filters' => [
+                ['name' => 'stripTags'],
+                ['name' => 'stringTrim'],
+            ],
+            'validators' => [
+                [
+                    'name' => StringLength::class,
+                    'options' => [
+                        'encoding' => 'UTF-8',
+                        'min' => 1,
+                        'max' => 100,
+                    ],
+                ],
+            ],
+        ]);
+        
+        $inputFilter->add([
+            'name' => 'title',
+            'required' => true,
+            'filters' => [
+                ['name' => 'stripTags'],
+                ['name' => 'stringTrim'],
+            ],
+            'validators' => [
+                [
+                    'name' => StringLength::class,
+                    'options' => [
+                        'encoding' => 'UTF-8',
+                        'min' => 1,
+                        'max' => 100,
+                    ],
+                ],
+            ],
+        ]);
+
+        $this->inputFilter = $inputFilter;
+        return $this->inputFilter;
     }
 }
