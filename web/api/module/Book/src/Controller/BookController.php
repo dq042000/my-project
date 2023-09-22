8bcd1d51ec109fe75065690466963320a84a2982
祁威德 <dq042000@gmail.com>
2023-09-22 14:57:39 +0800

    add cache.

--- web/api/module/Book/src/Controller/BookController.php
+++ web/api/module/Book/src/Controller/BookController.php
@@ -6,11 +6,22 @@ use Base\Controller\BaseController;
 
 class BookController extends BaseController 
 {
+    const ACL_CACHE_KEY = 'acl_service_cache_key';
+
     public function indexAction()
     {
         $cache = $this->getServiceManager()->get('cacheApc');
-        $cache->flush();
-        
+        // $cache->flush(); // 清除暫存
+        echo "Flushed<br>";
+        if ($cache->hasItem(self::ACL_CACHE_KEY)) {
+            $acl = $cache->getItem(self::ACL_CACHE_KEY);
+            echo $acl->format('Y-m-d H:i:s') . "-Cache<br>";
+        } else {
+            $acl = new \DateTime();
+            $cache->setItem(self::ACL_CACHE_KEY, new \DateTime());
+            echo $acl->format('Y-m-d H:i:s') . "-No cache<br>";
+        }
+        // $cache->removeItem(self::ACL_CACHE_KEY); // 移除暫存
         echo "Hello World!";
         exit;
     }
